2b003790cfc50d580c6903404268dc8c9e9f95d7
Mahavir717-virda <mahavirce7@gmail.com>
2026-07-24 17:25:56 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

---
+++ array/two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int,int> mp;
+        for (int i=0;i<nums.size();i++){
+            int a=nums[i];
+            int b=target-a;
+            if (mp.find(b)!=mp.end()) return {mp[b],i};
+            mp[a]=i;
+        }
+        return {-1,-1};
+    }
+};
