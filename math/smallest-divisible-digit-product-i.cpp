2354adc7f96b0756aa2a1d7ddee19beeca87a0a3
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-06 22:32:31 +0530

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

---
+++ math/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int y = INT_MAX;
+        vector<int> ans;
+        while (n <= 100) {
+            int n1 = n;
+            while (n1 > 0) {
+                int temp = n % 10;
+                ans.push_back(temp);
+                n1 /= 10;
+            }
+            if (ans.size() == 2) {
+                int x = ans[0] * ans[1];
+                if (x % t == 0) {
+                    y = min(y, n);
+                }
+            } else {
+                int x = ans[0] * ans[1] * ans[2];
+                if (x % t == 0) {
+                    y = min(y, n);
+                }
+            }
+            n++;
+        }
+
+        return y;
+    }
+};
