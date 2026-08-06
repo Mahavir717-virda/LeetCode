6b7dd6d0bffc6ec4fff2b79f8f8d2febcd019db9
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-06 22:37:31 +0530

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

--- enumeration/smallest-divisible-digit-product-i.cpp
+++ enumeration/smallest-divisible-digit-product-i.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        int y = INT_MAX;
-        vector<int> ans;
-        while (n <= 100) {
+        while(true)
+        {
             int n1 = n;
-            while (n1 > 0) {
-                int temp = n % 10;
-                ans.push_back(temp);
-                n1 /= 10;
+            int product = 1;
+            while(n1 > 0)
+            {
+                product *= (n1 % 10);
+                n1/=10;
             }
-            if (ans.size() == 1) {
-                if (ans[0] % t == 0) {
-                    y = min(y, n);
-                }
-            } else if (ans.size() == 2) {
-                int x = ans[0] * ans[1];
-                if (x % t == 0) {
-                    y = min(y, n);
-                }
-            } else {
-                int x = ans[0] * ans[1] * ans[2];
-                if (x % t == 0) {
-                    y = min(y, n);
-                }
+
+            if(product % t == 0)
+            {
+                return n;
             }
             n++;
         }
-
-        return y;
     }
 };
