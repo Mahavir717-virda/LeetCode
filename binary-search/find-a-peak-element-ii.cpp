e84d30052008c646a4303e4d9b59f0f84f9d69ae
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-11 23:57:26 +0530

    LC-find-a-peak-element-ii [Runtime: N/A; Memory: N/A]

--- binary-search/find-a-peak-element-ii.cpp
+++ binary-search/find-a-peak-element-ii.cpp
@@ -13,7 +13,8 @@ public:
         return idx;
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int low = 0, high = mat[0].size() - 1;
+        int n = mat.size(), m = mat[0].size(); 
+        int low = 0, high = m - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -21,7 +22,7 @@ public:
             int row = elementRow(mat, mid);
 
             int left = (mid - 1 >= 0) ? mat[row][mid - 1] : -1;
-            int right = mid + 1 < high ? mat[row][mid + 1] : -1;
+            int right = (mid + 1) < m ? mat[row][mid + 1] : -1;
 
             if (mat[row][mid] > left && mat[row][mid] > right) {
                 return {row, mid};
