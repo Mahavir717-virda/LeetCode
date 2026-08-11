8b6d6cc90418d1e832c4f7eedde6736aa72ab94a
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-11 21:45:31 +0530

    LC-smallest-missing-integer-greater-than-sequential-prefix-sum [Runtime: N/A; Memory: N/A]

---
+++ hash-table/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        set<int> s;
+        for(auto it : nums)
+            s.insert(it);
+
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] + 1 == nums[i]) {
+                sum += nums[i];
+            } 
+        }
+        int missing = sum;
+        
+        for(int i = missing; ; i++)
+        {
+            if(s.find(i) == s.end())
+            {
+                return i;
+            }
+        }
+    }
+};
