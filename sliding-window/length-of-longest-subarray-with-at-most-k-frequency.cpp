0ad6d0a52b05b85af85b9421737f4666001c9a12
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-12 21:20:33 +0530

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0, j = 0, maxLen = 0;
+        unordered_map<int, int> mp;
+        while (j < n) {
+            if ((mp.find(nums[j]) == mp.end()) or mp[nums[j]] < k) {
+                mp[nums[j]]++;
+                j++;
+            } else {
+                mp[nums[j]]++;
+                j++;
+                while (i < j and mp[nums[i]] > k) {
+                    mp[nums[i]]--;
+                }
+                i++;
+            }
+
+            maxLen = max(maxLen, j - i);
+            cout << maxLen << " ";
+        }
+
+        return maxLen;
+    }
+};
