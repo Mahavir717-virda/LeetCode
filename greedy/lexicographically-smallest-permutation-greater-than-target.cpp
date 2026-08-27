96b435c4ec9d54298218b70397d843e1e4232ab8
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-27 10:22:23 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- greedy/lexicographically-smallest-permutation-greater-than-target.cpp
+++ greedy/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -2,5 +2,13 @@ class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
         map<int,int> mps;
+        map<int,int> mpt;
+
+        for(int i = 0;i < s.size(); i++)
+        {
+            mps[s[i]]++;
+        }
+        
+
     }
 };
