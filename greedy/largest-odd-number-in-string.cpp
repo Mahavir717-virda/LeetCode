37640abb717210c14887104fb21dff355be164fa
Mahavir717-virda <mahavirce7@gmail.com>
2026-08-27 12:32:59 +0530

    LC-largest-odd-number-in-string [Runtime: N/A; Memory: N/A]

--- greedy/largest-odd-number-in-string.cpp
+++ greedy/largest-odd-number-in-string.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         string ans = "";
-        int temp = 0;
+        long long temp = 0;
         for (int i = 0; i < num.size(); i++) {
-            int n = num[i] - '0';
+            long long n = num[i] - '0';
             temp = (temp * 10) + n;
         }
-        int maxnum = INT_MIN;
+        long long maxnum = INT_MIN;
         while (temp > 0) {
             if (temp % 2 != 0) {
                 maxnum = max(maxnum, temp);
