e41e4acf9275bf175183c4517a4bde6b6ed8a464
Mahavir717-virda <mahavirce7@gmail.com>
2026-09-16 22:27:44 +0530

    LC-palindrome-linked-list [Runtime: 0 ms; Memory: 121 MB]

--- recursion/palindrome-linked-list.cpp
+++ recursion/palindrome-linked-list.cpp
@@ -1,37 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-
-        ListNode* curr = head;
+    ListNode* reverse(ListNode* newhead) {
+        ListNode* temp = newhead;
         ListNode* prev = nullptr;
-        while (curr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        ListNode* tail = nullptr;
+        while (temp != nullptr) {
+            ListNode* next = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = next;
         }
 
-        ListNode* rehead = prev;
-        ListNode* temp = head;
-        while(temp)
-        {
-            if(temp->val == rehead->val)
-            {
-                temp = temp->next;
-                rehead = rehead->next;
-            }
-            else
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* newhead = reverse(slow->next);
+
+        ListNode* first = head;
+        ListNode* second = newhead;
+
+        while (second != nullptr) {
+            if (first->val != second->val) {
+                reverse(newhead);
                 return false;
+            }
+
+            first = first->next;
+            second = second->next;
         }
 
         return true;
